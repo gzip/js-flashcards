c16b178140cd39eefd968a9fef2f4945ed50e847
Gamaiel Zavala <gamaiel@yahoo.com>
2018-05-27 22:21:02 -0700

    Clean up

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-var proxy = require('express-http-proxy');
 var express = require('express');
 var app = express();
 
 app.use(express.static('public', {etag: false, maxAge: 86400000}));
-//app.use('/proxy', proxy('www.google.com.mx', {https: true}));
-
-app.get('/', function(req, res){
-    res.send('hello world');
-});
-
 app.listen(80);
